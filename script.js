45b95fe17ea70d600b7f1636a0a1e3daf8d254cd
immorrtull <morrisontulloch@gmail.com>
2025-09-21 17:10:25 -0700

    main

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
 // Media items array
 const mediaItems = [
   { type: 'youtube', src: 'https://www.youtube.com/embed/s8MvS728gZs?si=m-XtIKuXbPsZY7IY', description: 'Documentary Video 1', section: 'documentary' },
-  { type: 'youtube', src: 'https://www.youtube.com/embed/qEefOJXaK1A?si=XdOVplP4ODCZhG-Z', description: 'Image 1', section: 'documentary' },
-  { type: 'youtube', src: 'https://www.youtube.com/embed/RWLSxyOfkWc?si=p2vjv9KhuCzJwY52', description: 'Image 1', section: 'documentary' },
+  { type: 'youtube', src: 'https://www.youtube-nocookie.com/embed/qEefOJXaK1A?si=XdOVplP4ODCZhG-Z', description: 'Image 1', section: 'documentary' },
+  { type: 'youtube', src: 'https://www.youtube-nocookie.com/embed/RWLSxyOfkWc?si=p2vjv9KhuCzJwY52', description: 'Image 1', section: 'documentary' },
   { type: 'youtube', src: 'https://www.youtube-nocookie.com/embed/h5vNnGchGic?si=nFV16A7bO2-NYu8A', description: 'Scripted Video 1', section: 'scripted' },
-  { type: 'youtube', src: 'https://www.youtube.com/embed/lZPTGkBCxlk?si=7HqHskuOw9PgQi9f', description: 'Image 2', section: 'scripted' },
-  { type: 'youtube', src: 'https://www.youtube.com/embed/jK5JKuz_g4c?si=Uuay9HNYGxjpkHMx', description: 'Social Media Video 1', section: 'scripted' },
-  { type: 'youtube', src: 'images/Image-1.png', description: 'Image 3', section: 'socialmedia' },
-  { type: 'youtube', src: 'images/Image-1.png', description: 'Podcasts Video 1', section: 'podcasts' },
-  { type: 'youtube', src: 'images/Image-1.png', description: 'Image 4', section: 'podcasts' },
-  { type: 'video', src: 'video2.mp4', description: 'Graphic Design Video 1', section: 'graphicdesign' },
-  { type: 'image', src: 'images/Image-1.png', description: 'Image 5', section: 'graphicdesign' },
+  { type: 'youtube', src: 'https://www.youtube-nocookie.com/embed/lZPTGkBCxlk?si=7HqHskuOw9PgQi9f', description: 'Image 2', section: 'scripted' },
+  { type: 'youtube', src: 'https://www.youtube-nocookie.com/embed/jK5JKuz_g4c?si=Uuay9HNYGxjpkHMx', description: 'Social Media Video 1', section: 'scripted' },
+  { type: 'instagram', src: 'DOBn5qwkTr4', description: 'Image 3', section: 'socialmedia' },
+  { type: 'youtube', src: 'https://www.youtube.com/embed/Zgv74NOQFaY"', description: 'Image 4', section: 'socialmedia' },
+  { type: 'instagram', src: 'DOg8xA6lPFi', description: 'Podcasts Video 1', section: 'socialmedia' },
+  { type: 'youtube', src: 'https://www.youtube.com/embed/HPZYNsEt-1Y?si=IWtzmrADDdfwN4HX', description: 'Graphic Design Video 1', section: 'podcasts' },
+  { type: 'youtube', src: 'https://www.youtube.com/embed/72gMCxgP2AE?si=t6cSdarAFhtNV-qN', description: 'Image 5', section: 'podcasts' },
+  { type: 'youtube', src: 'https://www.youtube.com/embed/mmyOswVxgng?si=Gm-dlDfovjvVYcNA', description: 'Image 5', section: 'podcasts' },
+  { type: 'image', src: 'images/Image-1.png', description: 'Image 5', section: 'multimedia' },
+  { type: 'image', src: 'images/Image-1.png', description: 'Image 5', section: 'multimedia' },
+  { type: 'image', src: 'images/Image-1.png', description: 'Image 5', section: 'multimedia' },
 ];
 
 // Lightbox elements
@@ -35,13 +39,15 @@ function populateGrids() {
     } else if (item.type === 'youtube') {
       mediaHTML = `<iframe class="media" src="${item.src}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen style="max-width: 100%; max-height: 100%;"></iframe>`;
     } else if (item.type === 'streamable') {
-  mediaHTML = `<iframe class="media" src="${item.src}"
-  allowfullscreen
-  height="100%"
-  width="100%"
-  style="border:none;
-  width: 100%;
-  height: 100%; position:absolute; left:0px; top:0px; overflow:hidden;"></iframe>`;
+      mediaHTML = `<iframe class="media" src="${item.src}"
+        allowfullscreen
+        height="100%"
+        width="100%"
+        style="border:none;
+        width: 100%;
+        height: 100%; position:absolute; left:0px; top:0px; overflow:hidden;"></iframe>`;
+    } else if (item.type === 'instagram') {
+      mediaHTML = `<iframe class="media" src="https://www.instagram.com/p/${item.src}/embed" width="400" height="480" frameborder="0" style="max-width: 100%; max-height: 100%;"></iframe>`;
     } else {
       mediaHTML = `<img class="media" src="${item.src}" alt="${item.description}" style="max-width: 100%; max-height: 100%;">`;
     }
@@ -82,6 +88,15 @@ function openLightbox(item) {
     iframe.frameborder = '0';
     iframe.allow = 'autoplay; encrypted-media';
     iframe.allowfullscreen = true;
+    iframe.style.maxWidth = '100%';
+    iframe.style.maxHeight = '100%';
+    lightboxContent.appendChild(iframe);
+  } else if (item.type === 'instagram') {
+    const iframe = document.createElement('iframe');
+    iframe.src = `https://www.instagram.com/p/${item.src}/embed`;
+    iframe.width = "400";
+    iframe.height = "480";
+    iframe.frameborder = "0";
     iframe.style.maxWidth = '100%';
     iframe.style.maxHeight = '100%';
     lightboxContent.appendChild(iframe);
